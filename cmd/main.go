1c0e514bf9419c41845a8782967cdcc264ad994d
Stanislav Luchankin <luchano057@gmail.com>
2024-02-29 19:27:08 +0700

    grpc api for USER

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,69 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
 
+	desc "github.com/Lokion13/auth-chat-server/pkg/user_v1"
+	"github.com/brianvoe/gofakeit"
 	"github.com/fatih/color"
+	"google.golang.org/grpc"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+// CREATE
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+// GET
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	return &desc.GetResponse{
+		User: &desc.User{
+			Id:        req.GetId(),
+			Name:      gofakeit.BeerName(),
+			Email:     gofakeit.Email(),
+			Role:      0,
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+// UPDATE
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
+// DELETE
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf(color.GreenString("server listening at %v", listen.Addr()))
+
+	if err = s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
